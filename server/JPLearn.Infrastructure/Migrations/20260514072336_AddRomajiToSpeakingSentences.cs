c2a4a03a2bfbcce059fc76f07f3cc670852a097b
cr7_dn <267825161+NXHuan@users.noreply.github.com>
2026-05-14 14:24:41 +0700

    fix: add speaking romaji migration

---
+++ server/JPLearn.Infrastructure/Migrations/20260514072336_AddRomajiToSpeakingSentences.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JPLearn.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRomajiToSpeakingSentences : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Romaji",
+                table: "speaking_sentences",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<long>(
+                name: "OrderCode",
+                table: "Orders",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Romaji",
+                table: "speaking_sentences");
+
+            migrationBuilder.DropColumn(
+                name: "OrderCode",
+                table: "Orders");
+        }
+    }
+}
